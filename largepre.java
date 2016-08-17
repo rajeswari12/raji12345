1956b750aa61c7edd1d77b0b3335043fde6d482b
rajeswari12 <rajarajeswari.marneedi@gmail.com>
2016-08-17 16:03:40 +0530

    largepre.java

---
+++ largepre.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+class largepre
+{
+public static String longestCommonPrefix(String[] strings)
+{
+if (strings.length == 0)
+{
+return "";    
+}
+for (int prefixLen = 0; prefixLen < strings[0].length(); prefixLen++) 
+{  
+char c = strings[0].charAt(prefixLen);
+for (int i = 1; i < strings.length; i++) {
+if ( prefixLen >= strings[i].length() ||strings[i].charAt(prefixLen) != c ) 
+{                
+return strings[i].substring(0, prefixLen);  
+}  
+}
+}
+return strings[0];
+}
